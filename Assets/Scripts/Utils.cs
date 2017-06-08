a90b0b959a42f3385b02def604d1d1f004ab3c5e
wenluzhizhi <fang10159066099@163.com>
2017-06-08 14:59:37 +0800

    chagne

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+using System;
+
+public class Utils
+{
+
+	public static int getRandom1()
+	{
+
+		byte[] randomBytes = new byte[4];
+		RNGCryptoServiceProvider rngCrypto =
+			new RNGCryptoServiceProvider();
+		rngCrypto.GetBytes(randomBytes);
+		int rngNum = BitConverter.ToInt32(randomBytes, 0);
+		return rngNum;
+	}
+}
--- Assets/myGame/Scripts/AI/AITank.cs
+++ Assets/myGame/Scripts/AI/AITank.cs
@@ -2,20 +2,69 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class AITank : MonoBehaviour,AIEnemy 
+public class AITank : NetworkBehaviour,AIEnemy 
 {
-	public void Attack(){}
+	public void Attack(){
+
+		//CmdAttack ();
+	}
 	public void Sleep(){}
 
 
-	public void Front(){
-		//this.transform.Translate (transform.forward*Time.deltaTime*Random.Range(-0.5f,1f));
-	}
-
-
-	void  Update(){
+	public void Front()
+	{
 		
+
 	}
+
+
+	private int sleepThreshold = 5;
+	private int AwakesleepThreshold = 95;
+	private bool isSleep=false;
+	public int random0_100 = 0;
+	public Transform bulletGeneratePos;
+	public GameObject Bullet;
+	void  Update()
+	{
+		random0_100 = Mathf.Abs(Utils.getRandom1() % 100);
+		if (random0_100 < sleepThreshold)
+		{
+			if (isSleep == false) {
+				isSleep = true;
+				this.transform.Rotate(new Vector3(0,random0_100%45,0));
+			}
+
+		}
+		if (random0_100 > AwakesleepThreshold) {
+			if (isSleep)
+				isSleep = false;
+		}
+
+		if (random0_100 > 95) {
+			//Attack ();
+			if(NetworkManager.singleton.isNetworkActive){
+				CmdAttack1();
+			}
+
+		}
+
+
+		if(!isSleep)
+		  this.transform.Translate (transform.forward*Time.deltaTime,Space.World);
+
+	}
+
+
+   [Command]
+	public void CmdAttack1()
+	{
+		GameObject _bullet = GameObject.Instantiate (Bullet,bulletGeneratePos.position,Quaternion.identity) as GameObject;
+		_bullet.gameObject.GetComponent<Rigidbody> ().velocity = transform.forward * 60;
+		Destroy (_bullet,2);
+		//NetworkServer.Spawn (_bullet);
+	}
+
+
 
 
 }
